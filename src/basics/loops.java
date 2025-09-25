88a2df5a4a2b254d0ac5ba04a8361b34d08497c7
Amardeep <vermaamardeep86@gmail.com>
2025-09-26 04:08:29 +0530

    Merge branch 'main' of https://github.com/Amardeep-Verma/Java

---
+++ src/basics/loops.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class loops {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+
+        for (int i = 1; i <= n; i++) {
+            // print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); // new line
+        }
+    }
+}
